713f026f4a2a0930bc627cb2777c4ae3c2692ee4
elbaable <trahughes1026@gmail.com>
2025-01-20 11:18:21 -0800

    update homepage style

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 type Task = {
   id: number;
@@ -56,55 +57,108 @@ const Home = ({ tasks }: Props) => {
   const completedTasks = taskList.filter((task) => task.completed).length;
 
   return (
-    <div className="p-8">
-      <h1 className="text-3xl mb-4">Todo List</h1>
-      <Link href="/task/new" className="btn btn-primary">
-        Create Task
-      </Link>
-
-      {/* Display total tasks and completed tasks */}
-      <div className="mt-4 mb-6">
-        <p className="text-lg">
-          Tasks {totalTasks} | Completed {completedTasks}
-        </p>
+    <div className="bg-backgroundColor">
+      <div className="h-[200px] bg-headerColor">
+        <div className="p-48px text-[40px] font-extraHeavy flex justify-center align-center">
+          <Image
+            src="/img/rocket.png"
+            alt="Todo App Rocket"
+            width={20}
+            height={20}
+            style={{width: '30px', height: '45px', marginTop: '10px'}}
+          />
+          <span className="text-customBlue ml-2">Todo</span>
+          <span className="text-customPurple ml-2">App</span>
+        </div>
+      </div>
+      <div className="flex justify-center mt-[-26px]">
+        <Link href="/task/new">
+          <div className="btn flex justify-center items-center bg-customBtn w-[736px] h-[52px] rounded-[8px] text-white text-[14px]">
+            <span className="mr-2">Create Task</span>
+            <Image
+              src="/img/add.png"
+              alt="Add a new task"
+              width={20}
+              height={20}
+            />
+          </div>
+        </Link>
       </div>
 
-      {totalTasks === 0 ? (
-        <div>
-            <p className="text-lg text-gray-500">You don't have any tasks registered yet.</p>
-            <p className="text-lg text-gray-500">Create tasks and organize your to-do items.</p>
+      <div className="w-[736px] min-h-[511px] m-auto py-10">
+        {/* Display total tasks and completed tasks */}
+        <div className="flex justify-between mb-6">
+          <div><span className="text-customBlue mr-2">Tasks</span> <span className="rounded-full bg-customBadge text-customBadgeText px-2">{totalTasks}</span></div>
+          <div><span className="text-customBlue mr-2">Completed</span> <span className="rounded-full bg-customBadge text-customBadgeText px-2">{completedTasks} de {totalTasks}</span></div>
         </div>
-      ) : (
-        <div className="mt-4">
-            <ul>
-            {taskList.map((task) => (
-                <li key={task.id} className="flex items-center justify-between p-2 border-b">
-                <Link 
-                    href={`/task/${task.id}`}
-                    style={{ color: task.color }}
-                    className={task.completed ? 'line-through' : ''}
-                >
-                    {task.title}
-                </Link>
-                <div>
+
+        {totalTasks === 0 ? (
+          <div className="text-center">
+              <Image
+                src="/img/Clipboard.png"
+                alt="No task"
+                width={56}
+                height={56}
+                style={{margin: '20px auto'}}
+              />
+              <div className="text-lg text-noTaskText font-bold mb-6">You don't have any tasks registered yet.</div>
+              <div className="text-lg text-noTaskText font-middle">Create tasks and organize your to-do items.</div>
+          </div>
+        ) : (
+          <div className="mt-4">
+              <ul>
+              {taskList.map((task) => (
+                  <li key={task.id} className="flex items-center justify-between p-2 border-b">
                     <button
-                    onClick={() => handleToggleCompletion(task.id, task.completed)}
-                    className="mr-2 p-1 bg-blue-500 text-white"
-                    >
-                    {task.completed ? 'Mark as Incomplete' : 'Mark as Complete'}
+                      onClick={() => handleToggleCompletion(task.id, task.completed)}
+                      className="mr-2"
+                      >
+                      {task.completed ? (
+                        <div className="w-[24px]">
+                          <Image
+                            src="/img/check.png"
+                            alt="Completed task"
+                            width={24}
+                            height={24}
+                          />
+                        </div>
+                      ) : (
+                        <div className="w-[24px]">
+                          <Image
+                            src="/img/uncheck.png"
+                            alt="Umcompleted task"
+                            width={24}
+                            height={24}
+                          />
+                        </div>
+                      )}
                     </button>
-                    <button
-                    onClick={() => handleDelete(task.id)}
-                    className="p-1 bg-red-500 text-white"
+                    <Link 
+                        href={`/task/${task.id}`}
+                        style={{ color: task.color }}
+                        className={task.completed ? 'line-through opacity-50' : ''}
                     >
-                    Delete
-                    </button>
-                </div>
-                </li>
-            ))}
-            </ul>
-        </div>
+                        {task.title}
+                    </Link>
+                      <button
+                      onClick={() => handleDelete(task.id)}
+                      className="ml-2"
+                      >
+                        <div className="w-[24px]">
+                          <Image
+                            src="/img/remove.png"
+                            alt="Remove task"
+                            width={24}
+                            height={24}
+                          />
+                        </div>
+                      </button>
+                  </li>
+              ))}
+              </ul>
+          </div>
         )}
+        </div>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "pages/**/*.{js,ts,jsx,tsx}",
+    "components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerColor: "#0D0D0D",
+        backgroundColor: "#1A1A1A",
+        customBlue: "#4EA8DE",
+        customPurple: "#5E60CE",
+        customBtn: "#1E6F9F",
+        customBadge: "#333333",
+        customBadgeText: "#D9D9D9",
+        noTaskText: "#808080",
+      },
+      spacing: {
+        "16px": "16px",
+        "48px": "48px",
+      },
+      fontWeight: {
+        middle: "400",
+        bold: "700",
+        extraHeavy: "900",
+      },
+    },
   },
   plugins: [],
 };
